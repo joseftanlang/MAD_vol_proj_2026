8e54387a1ea67dfe567d3bce3cd892e5ce05baff
superken <CKENDRICSES06.24@ichat.sp.edu.sg>
2026-02-06 17:58:33 +0800

    Added in font size provider!!

--- lib/accessibility_provider.dart
+++ lib/accessibility_provider.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 // Use provider version: ^6.1.2
 // Use shared_preferences: ^2.3.2
 class AccessibilityProvider extends ChangeNotifier {
-
-  static TextTheme textVariations = TextTheme(
+  
+  TextTheme _textVariations = TextTheme(
     bodyMedium: TextStyle(
       fontSize: 16,
     ),
@@ -16,8 +16,25 @@ class AccessibilityProvider extends ChangeNotifier {
       fontWeight: FontWeight.bold
     ),
   );
+  TextTheme get textVariations => _textVariations;
+  void changeFontSize(double changedValue) {
+    _textVariations = TextTheme(
+        bodyMedium: TextStyle(
+        fontSize: 16*changedValue,
+      ),
+      titleMedium: TextStyle(
+        fontSize: (16+4)*changedValue,
+        fontWeight: FontWeight.bold
+      ),
+      headlineMedium: TextStyle(
+        fontSize: (16+8)*changedValue,
+        fontWeight: FontWeight.bold
+      ),
+    );
+    notifyListeners();
+  }
   // ThemeMode currentContrast = ThemeMode.dark;
-  ThemeMode _darkMode = ThemeMode.dark;
+  ThemeMode _darkMode = ThemeMode.light;
   ThemeMode get darkMode => _darkMode;
   // Function to change app contrast!!
   void changeContrast(ThemeMode changedValue) {
